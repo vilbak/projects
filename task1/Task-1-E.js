09dc3b98339c534370c5f21307a1690efd056aa9
vilbak <vilbak86@gmail.com>
2019-05-22 14:07:13 +0300

    Reworked names

--- task1/Task-1-E.js
+++ task1/Task-1-E.js
@@ -1,10 +1,11 @@
-let maxNumber = (array) =>{
+//названия 
+let getMaxNumber = (array) =>{
     let newArr = array;
- let redu = (element) => (element.reduce((max ,current) => max > current ? max : current )) ;
+ let filterTheArray = (arr) => (arr.reduce((max ,current) => max > current ? max : current )) ;
  
-  return redu(newArr);
+  return filterTheArray(newArr);
 }
 
 let ratings = [2,3,1,4,5];
 
-console.log(maxNumber(ratings));
+console.log(getMaxNumber(ratings));
