f0b619166b8de6e8f708d8f5a7c43680bf393b05
vilbak <vilbak86@gmail.com>
2019-05-22 14:12:11 +0300

    Output the result of sum 0 +1

---
+++ task2/Task 2-A.js
@@ -0,0 +1,14 @@
+let Robot = function (name) {
+    this.name = name;
+}
+
+function add(op1, op2) {
+    this.name = this.name || "Human";
+    return this.name + " can count to " + (op1 + op2);
+}
+
+let voltron = new Robot("Voltron");
+
+let human = add(0,1);
+
+console.log(human);
