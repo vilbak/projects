26909d79196ff84f7460e4bd5d904c17a0838ce9
vilbak <vilbak86@gmail.com>
2019-05-22 14:31:56 +0300

    Output with the help of function apply

--- task2/Task 2-C.js
+++ task2/Task 2-C.js
@@ -1,4 +1,4 @@
-let Robot = function (name) {
+let defineName = function (name) {
     this.name = name;
 }
 
@@ -7,6 +7,6 @@ function add(op1, op2) {
     return this.name + " can count to " + (op1 + op2);
 }
 
-let voltron = new Robot("Voltron");
+let voltron = new defineName("Voltron");
 
 console.log(add.apply(voltron,[20,30]));
