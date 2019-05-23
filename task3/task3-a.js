a302125de80ac9eea43b5c93e154f872eedf0301
Vilbak <vilbak86@gmail.com>
2019-05-23 18:48:25 +0300

    button output "Hello world"

---
+++ task3/task3-a.js
@@ -0,0 +1,7 @@
+ let button = document.querySelector("button");
+
+ button.onclick  = () =>{
+     let timer;
+     timer &&clearTimeout(timer);
+     timer = setTimeout(()=>{console.log("Hello world")},5000)
+ };
