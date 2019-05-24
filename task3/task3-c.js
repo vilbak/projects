c7665a5a6215de6d1b8b3ddd40f1987ba06ea481
vilbak <vilbak86@gmail.com>
2019-05-24 13:32:43 +0300

    button "You have waited" +seconds

---
+++ task3/task3-c.js
@@ -0,0 +1,22 @@
+let button = document.querySelector(".task-c");
+
+let randomTimer;
+let getRandomValue = (min, max) =>
+    Math.round(Math.random() * (max - min)) + min;
+
+let createRandomTimeout = (seconds) => {
+    console.log(`You have waited ${seconds} seconds`);
+    randomTimer = setTimeout(() => {
+        clearTimeout(randomTimer);
+
+    }, seconds * 1000)
+}
+button.addEventListener('click', () => {
+    if (randomTimer) {
+        clearTimeout(randomTimer);
+        
+    } else {
+        createRandomTimeout(getRandomValue(1, 4));
+    }
+})
+
