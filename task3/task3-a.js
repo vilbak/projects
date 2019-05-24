7a41abc04edc62e0fc645201cf51e632331d5fda
vilbak <vilbak86@gmail.com>
2019-05-24 13:31:33 +0300

    Button with the delay of 5 sec

--- task3/task3-a.js
+++ task3/task3-a.js
@@ -1,7 +1,9 @@
- let button = document.querySelector("button");
+let button = document.querySelector(".task-a");
 
- button.onclick  = () =>{
-     let timer;
-     timer &&clearTimeout(timer);
-     timer = setTimeout(()=>{console.log("Hello world")},5000)
- };
+button.onclick = () => {
+    let timer;
+    timer && clearTimeout(timer);
+    timer = setTimeout(() => {
+        console.log("Hello world")
+    }, 5000)
+};
