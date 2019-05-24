83c0d8f6d8aa293d834d170d4feeac759f4ab3b9
vilbak <vilbak86@gmail.com>
2019-05-24 13:32:09 +0300

    button "You are welcome" with the delay of 3 sec

---
+++ task3/task3-b.js
@@ -0,0 +1,13 @@
+let taskB = document.querySelector(".task-b");
+
+let timer;
+
+taskB.addEventListener('click', () => {
+    if (timer) {
+        clearInterval(timer);
+    } else {
+        timer = setInterval(() => {
+            console.log("You are welcome")
+        }, 3000)
+    }
+})
