dd42d7594c55c9b75c64a195393795e9e6c93ee4
Vilbak <vilbak86@gmail.com>
2019-05-26 16:57:44 +0300

    delete because of mistakes

--- task2/Task 2-D.js
+++ task2/Task 2-D.js
@@ -1,33 +1,18 @@
-/*let defineName = function (name) {
-    this.name = name;
-     this.alertName  = function(method){
-        console.log(this.name + ' '+ method);
-    }
+
+
+let name = 'Walle';
+
+
+
+function showName(name) {
+    this.name = name
+    setTimeout(() => {
+        console.log(this.name)
+       
+    }, 0)
 }
 
-let name = new defineName('Walle');
-
-console.log(name.alertName.call(name, 'call'));
-
-console.log(name.alertName.apply(name, ['apply']));
 
 
- let newName = name.alertName.bind(name);
 
-console.log(newName('bind'));
-
-
-let outputTheName = function (name) {
-    let that = this;
-    that.name = name;
-    that.alertName = function (method) {
-        console.log(that.name + ' ' + method);
-    }
-}
-let Newname = new outputTheName('Walle');
-
-console.log(name.alertName.apply(name, ['apply'])); */
-
-function showName() {
-    setTimeout(() =>)
-}
+showName()
