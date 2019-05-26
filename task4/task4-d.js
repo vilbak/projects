7b648add3041f968d2cdbf9d0e4a692aabe87e44
Vilbak <vilbak86@gmail.com>
2019-05-26 16:54:53 +0300

    generate from 1 to 10 functions

--- task4/task4-d.js
+++ task4/task4-d.js
@@ -1,13 +1,18 @@
-let randomNumber = (min, max) => (Math.round(Math.random() * (max - min)));
+let randomNumber = (min, max) => (Math.round(Math.random() * (max - min) + min));
 
-let generateFunctions = (index,min,max) =>{
-    new Promise((resolve ,reject) =>{
-        let time =randomNumber(min,max);
-        setTimeout(()=>{
-            console.log("function" +index +"time" +time) ;
+const arrayOfPromises = [];
 
-            resolve("all good")
-
-        },time)
+for (let index = 0; index < randomNumber(1, 10); index++) {
+    const promise = new Promise((resolve) => {
+        let time = randomNumber(100, 1000);
+        setTimeout(() => {
+            console.log("function " + index + " time " + time);
+            resolve("done")
+        }, time)
     })
+    arrayOfPromises.push(promise)
 }
+
+Promise.all(arrayOfPromises).then(values => {
+    console.log("well done ")
+})
