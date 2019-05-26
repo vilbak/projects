df7f2cd074140c96fead0e74a94d9e1d7943b684
Vilbak <vilbak86@gmail.com>
2019-05-26 16:53:40 +0300

    first task has been reworked

---
+++ task1/task-D.js
@@ -0,0 +1,64 @@
+Array.prototype.filter = function (funcFilter) {
+    let newArr = [];
+    for (let i = 0; i < this.length; i++) {
+        funcFilter(this[i], i) && newArr.push(this[i]);
+
+    }
+    return newArr;
+};
+let newReleases = [{
+        "id": 70111470,
+        "title": "Die Hard",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [4.0],
+        "bookmark": []
+    },
+    {
+        "id": 654356453,
+        "title": "Bad Boys",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [5.0],
+        "bookmark": [{
+            id: 432534,
+            time: 65876586
+        }]
+    },
+    {
+        "id": 65432445,
+        "title": "The Chamber",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [4.0],
+        "bookmark": []
+    },
+    {
+        "id": 675465,
+        "title": "Fracture",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [5.0],
+        "bookmark": [{
+            id: 432534,
+            time: 65876586
+        }]
+    }
+];
+
+let task = (function () {
+    const getIds = (list) => (list
+        .filter(({
+                rating
+            }) =>
+            rating.every(val => val === 5)
+        )
+        .map(({
+            id
+        }) => id)
+    )
+    return getIds;
+})();
+
+
+console.log(task(newReleases));
---
+++ task1/task-E.js
@@ -0,0 +1,23 @@
+Array.prototype.reduce = function (combiner, initialValue) {
+  let accumulator = initialValue || this.shift();
+  for (let i = 0; i < this.length; i++) {
+      accumulator = combiner(accumulator, this[i]);
+  }
+  return accumulator;
+}
+let  ratings = [2, 3, 1, 4, 5];
+    
+let findMaxNumber = (function () {
+
+    const max = (ratings) => ratings.reduce((max, current) => max > current ? max : current);
+
+    return max;
+}());
+
+console.log(findMaxNumber(ratings));
+
+
+
+
+
+
