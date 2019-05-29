28436ed2cd88db6091b9448e17ef6c1444eca83a
vilbak <vilbak86@gmail.com>
2019-05-29 13:47:46 +0300

    Updates

---
+++ precache-manifest.d0b507f4808ef4b220f3dc3e18c8a45a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867ca05e1dbdbf2064b10951da2d5886",
+    "url": "/projects/./index.html"
+  },
+  {
+    "revision": "55d20a59c064e7f69100",
+    "url": "/projects/./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "aa484b427497684fece9",
+    "url": "/projects/./static/js/2.82832123.chunk.js"
+  },
+  {
+    "revision": "55d20a59c064e7f69100",
+    "url": "/projects/./static/js/main.9d718129.chunk.js"
+  },
+  {
+    "revision": "6b7f39c3d73c85229281",
+    "url": "/projects/./static/js/runtime~main.3b7fb317.js"
+  }
+]);
