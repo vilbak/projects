e231e63f2dad4d272e3d1e511f98ee5703918ae5
Vilbak <vilbak86@gmail.com>
2019-06-04 20:18:08 +0300

    render

--- task2/src/App.js
+++ task2/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
-import FixedContainer from  './Containers/MainElement'
-import SwithcesGroup from './Containers/subContainer'
-
-
+import Handler from './Container/ContainerHandler';
 
 class App extends React.Component{
   render (){
-    return (<div >
-      <FixedContainer/>,
-      <SwithcesGroup />
-      </div>)
+    return <Handler /> 
   }
 }
 export default App
